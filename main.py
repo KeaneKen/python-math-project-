761977e9c80ec23b8d2a7afc6bb62a979613a469
EcksdeeCoconut <faatihkeane@gmail.com>
2024-11-08 21:01:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+while True:
+    eq = input('Apa yang anda mau\n1.Pertambahan\n2.Perkurangan\n3.Perkalian\n4.Pembagian\n')
+    try:
+        equ = int(eq)
+        if equ in [1, 2, 3, 4]:
+            break
+        else:
+                print('Tidak Valid')
+    except ValueError :
+        print('Tidak Valid')
+    
+n1 = input('masukkan angka pertama ')
+n2 = input('masukkan angka kedua ')
+
 
